54ed3dc3ba71a79749149b2738c2cfd3fdd6c0c8
Andy Cockerill <34304883+andycockers@users.noreply.github.com>
2020-03-03 20:49:51 +0000

    Update vars.sh
    Removed secrets

--- sr_devops/andyc/ansible/vars.sh
+++ sr_devops/andyc/ansible/vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export EC2_ACCESS_KEY=AKIA2VVNWF4URX2OHE7Y
-export EC2_SECRET_KEY="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"
+export EC2_ACCESS_KEY=
+export EC2_SECRET_KEY=""
 export EC2_URL=https://ec2.amazonaws.com
 export S3_URL=https://s3.amazonaws.com:443
 export AWS_ACCESS_KEY_ID=${EC2_ACCESS_KEY}
